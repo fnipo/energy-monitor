e5c51d40a73f7ba2951b0da3dd2d257c4ad51f3f
Felipe Nipo <fpnipo@gmail.com>
2016-06-07 17:28:51 -0300

    Initial implementation of FFT chart.

--- viewer/public/Monitor/EmonChartDirective.js
+++ viewer/public/Monitor/EmonChartDirective.js
@@ -3,8 +3,8 @@ app.directive('emonChart', ['$templateCache', function($templateCache) {
     return {
         restrict: 'A',
         link: function(scope, elem, attrs) {
-            var chart = new Chart(elem, scope.emonChart);
-            scope.emonChart.chartObject = chart;
+            var chart = new Chart(elem, scope.emonChart[attrs.id]);
+            scope.emonChart[attrs.id].chartObject = chart;
         }
     }
 }]);
--- viewer/public/Monitor/MonitorController.js
+++ viewer/public/Monitor/MonitorController.js
@@ -1,9 +1,13 @@
 var app = angular.module('EnergyMonitor.Monitor');
 app.controller('MonitorController', function ($scope) {
 	
+	var fft = load('fft-js').fft;
+	var fftUtil = load('fft-js').util;
+	
 	var labelsArray = [];
 	
-	$scope.emonChart = {
+	$scope.emonChart = [];
+	$scope.emonChart[0] = {
 		type: 'line',
 		data: {
 			labels: labelsArray,
@@ -45,7 +49,8 @@ app.controller('MonitorController', function ($scope) {
 						display: false
 					},
 					ticks: {
-						display: false,
+						//display: false,
+						suggestedMax: 300
 					}
 				}]
 			}
@@ -53,14 +58,84 @@ app.controller('MonitorController', function ($scope) {
 		
 		chartObject: {},
 		updateDataBuffer: function(value) {
-			var dataBuffer = $scope.emonChart.data.datasets[0].data; 
-			if (dataBuffer.length >= 500) {
+			var dataBuffer = this.data.datasets[0].data; 
+			if (dataBuffer.length >= 300) {
 				dataBuffer.shift();
 			} else {
-				labelsArray.push('');
+				labelsArray.push(dataBuffer.length);
 			}
 			dataBuffer.push(value);
-			$scope.emonChart.chartObject.update();
+			this.chartObject.update();
+		}
+	};
+	
+	$scope.emonChart[1] = {
+		type: 'line',
+		data: {
+			labels: labelsArray,
+			datasets: [{
+				label: 'FFT',
+				data: [],
+				baseData: [],
+				backgroundColor: [
+					'rgba(255, 99, 132, 0.2)',
+					'rgba(54, 162, 235, 0.2)',
+					'rgba(255, 206, 86, 0.2)',
+					'rgba(75, 192, 192, 0.2)',
+					'rgba(153, 102, 255, 0.2)',
+					'rgba(255, 159, 64, 0.2)'
+				],
+				borderColor: [
+					'rgba(255,99,132,1)',
+					'rgba(54, 162, 235, 1)',
+					'rgba(255, 206, 86, 1)',
+					'rgba(75, 192, 192, 1)',
+					'rgba(153, 102, 255, 1)',
+					'rgba(255, 159, 64, 1)'
+				],
+				borderWidth: 3,
+				fill: false,
+				lineTension: 0,
+				pointRadius: 0
+			}]
+		},
+		options: {
+			scales: {
+				yAxes: [{
+					ticks: {
+						beginAtZero: true,
+						suggestedMax: 15e62
+					}
+				}],
+				xAxes: [{
+					gridLines: {
+						display: false
+					},
+					ticks: {
+						beginAtZero: true,
+						suggestedMax: 4000
+					}
+				}]
+			}
+		},
+		
+		chartObject: {},
+		updateFFTBuffer: function(value) {
+			var dataBuffer = this.data.datasets[0].baseData; 
+			if (dataBuffer.length >= 300) {
+				dataBuffer.shift();
+			}
+			dataBuffer.push(value);
+			
+			var phasors = fft(dataBuffer);
+			// sampleRate: 100 samples per second
+			var frequencies = fftUtil.fftFreq(phasors, 100);
+    		var magnitudes = fftUtil.fftMag(phasors);
+			
+			this.data.labels = frequencies.slice();
+			this.data.datasets[0].data = magnitudes.slice();
+			
+			this.chartObject.update();
 		}
 	};
 	
@@ -71,7 +146,8 @@ app.controller('MonitorController', function ($scope) {
 	ipc.config.silent = true;
     ipc.serve(function () {        
 		ipc.server.on('emonserial:data', function (data, socket) {
-			$scope.emonChart.updateDataBuffer(data.current);
+			$scope.emonChart[0].updateDataBuffer(data.current);
+			$scope.emonChart[1].updateFFTBuffer(data.current);
             $scope.$apply();
 		});
 	});
