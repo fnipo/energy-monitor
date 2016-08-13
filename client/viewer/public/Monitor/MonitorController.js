9fa4c53a224e1fe8af070b720008425aa7d8fd7a
Felipe Nipo <fpnipo@gmail.com>
2016-08-13 17:50:23 -0300

    Adding data logging functionality. And adjusting FFT sample rate

--- client/viewer/public/Monitor/MonitorController.js
+++ client/viewer/public/Monitor/MonitorController.js
@@ -9,6 +9,20 @@ app.controller('MonitorController', function ($scope) {
 	
 	var labelsArray = [];
 	
+	var fs = require('fs');
+	var energyLogFile = fs.createWriteStream('energylog' + Date.now() + '.txt',
+		{ flags: 'a' });
+	// Or 'w' to truncate the file every time the process starts.
+	energyLogFile.write('Energy Log:');
+	energyLogFile.write('\r\n');
+	energyLogFile.write('\r\n');
+	
+	var fftLogFile = fs.createWriteStream('fftlog' + Date.now() + '.txt',
+		{ flags: 'a' });
+	// Or 'w' to truncate the file every time the process starts.
+	fftLogFile.write('FFT Log:');
+	fftLogFile.write('\r\n');
+	
 	$scope.data = 10;
 	
 	$scope.emonChart = [];
@@ -129,7 +143,12 @@ app.controller('MonitorController', function ($scope) {
 			{
 				var phasors = fft(dataBuffer.slice());
 				// sampleRate: 3.79 samples per second, 1 sample each 264ms
-				var frequencies = fftUtil.fftFreq(phasors, 3.79);
+				//var frequencies = fftUtil.fftFreq(phasors, 3.79);
+				// sampleRate: 2 samples per second, 1 sample each 500ms
+				var frequencies = fftUtil.fftFreq(phasors, 2);
+				frequencies = frequencies.slice().map(function(val, i) {
+					return Math.round(val * 10) / 10;
+				});
 				var magnitudes = fftUtil.fftMag(phasors);
 				magnitudes = magnitudes.slice().map(function(val, i) {
 					return isNaN(val) ? 0 : val; 
@@ -137,6 +156,8 @@ app.controller('MonitorController', function ($scope) {
 				
 				this.data.labels = frequencies.slice();
 				this.data.datasets[0].data = magnitudes;
+				
+				logFFT(dataBuffer, frequencies, magnitudes);
 				
 				this.chartObject.update();
 			}
@@ -153,6 +174,7 @@ app.controller('MonitorController', function ($scope) {
 			$scope.emonChart[0].updateDataBuffer(data.current);
 			$scope.emonChart[1].updateFFTBuffer(data.current);
 			$scope.data = data.current;
+			energyLogFile.write(data.current + ', ');
             $scope.$apply();
 		});
 	});
@@ -161,6 +183,31 @@ app.controller('MonitorController', function ($scope) {
 	
 	var isPowerOf2 = function(value) {
 		return ((value & (value - 1)) == 0);
+	}
+	
+	var logFFT = function(samples, frequencies, magnitudes) {
+		fftLogFile.write('\r\n');
+		fftLogFile.write('\r\n');
+		
+		fftLogFile.write('Samples array (size: ' + samples.length + '): \n');
+		samples.forEach(function(element) {
+			fftLogFile.write(element + ', ');
+		}, this);
+		fftLogFile.write('\r\n');
+		
+		fftLogFile.write('Frequencies array: \n');
+		frequencies.forEach(function(element) {
+			fftLogFile.write(element + ', ');
+		}, this);
+		fftLogFile.write('\r\n');
+		
+		fftLogFile.write('Magnitudes array: \n');
+		magnitudes.forEach(function(element) {
+			fftLogFile.write(element + ', ');
+		}, this);
+		fftLogFile.write('\r\n');
+		
+		fftLogFile.write('\n\n\n');
 	}
 	
 });
